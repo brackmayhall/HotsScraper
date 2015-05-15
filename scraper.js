fd689fc243ba916c33fad643fa9ed6657effdc5e
Brack Mayhall <brackmayhall@gmail.com>
2015-05-14 22:04:04 -0400

    initial scraper commit

---
+++ scraper.js
@@ -0,0 +1,42 @@
+var phantom = require('phantom');
+var fs = require('fs');
+var select = require('soupselect').select;
+var htmlparser = require("htmlparser");
+var sys = require('sys');
+
+
+phantom.create(function (ph) {
+
+  ph.createPage(function (page) {
+    page.open("http://us.battle.net/heroes/en/heroes/", function (status) {
+      console.log("opened hots? ", status);
+      page.evaluate(function () { return document.documentElement.innerHTML; }, function (result) {
+
+        // now we have the whole body, parse it and select the nodes we want...
+        var handler = new htmlparser.DefaultHandler(function(err, dom) {
+            if (err) {
+                console.log("Error: " + err);
+            } else {
+
+                var heroes = select(dom, '.hero-list__item a');
+
+                heroes.forEach(function(hero) {
+					console.log('Name: ' + hero.attribs['data-action']);
+					console.log('Slug: ' + hero.attribs['href']);
+                })
+            }
+        });
+
+		var parser = new htmlparser.Parser(handler);
+		parser.parseComplete(result);
+
+        ph.exit();
+      });
+    });
+  });
+  
+}, {
+  dnodeOpts: {
+    weak: false
+  }
+});
